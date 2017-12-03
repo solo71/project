05023a97b5a5f037181b0e99ccb86f34d7a814a6
solo7 <solo77.job@gmail.com>
2017-12-03 22:21:12 +0300

    Переход на Asset Packagist

--- config/console.php
+++ config/console.php
@@ -1,20 +1,12 @@
 <?php
 Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
 $config = [
-    'id' => 'basic-console',
+    'id' => 'app-console',
     'bootstrap' => ['gii'],
     'controllerNamespace' => 'app\commands',
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
-    'params' => $params,
-    /*
-    'controllerMap' => [
-        'fixture' => [ // Fixture generation command line.
-            'class' => 'yii\faker\FixtureController',
-        ],
-    ],
-    */
 ];
 
 
--- config/web.php
+++ config/web.php
@@ -1,33 +1,29 @@
 <?php
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'id' => 'app',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
-        'request' => [
-            'cookieValidationKey' => '',
-        ],
         'user' => [
             'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-         ],
-   ],
+        ],
+    ],
 ];
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] =  'yii\debug\Module';
+    $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
