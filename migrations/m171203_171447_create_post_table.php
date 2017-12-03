21f861d8d5bfd2338d90936625f30cae2b084dc8
solo7 <solo77.job@gmail.com>
2017-12-03 20:43:47 +0300

    base_create

---
+++ migrations/m171203_171447_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `post`.
+ */
+class m171203_171447_create_post_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        $this->createTable('post', [
+            'id' => $this->primaryKey(),
+            'date' => $this->integer()->notNull(),
+            'category_id' => $this->integer()->notNull(), //Номер категории
+            'text' => $this->text()->notNull(),
+            'title' => $this->string()->notNull()->unique(), // Название статьи
+            'abridgment' => $this->text()->notNull(), // Сокращенный текст
+            'activity' => $this->integer()->notNull()->defaultValue(0), // Активность статьи
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('post');
+    }
+}
---
+++ migrations/m171203_171523_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m171203_171523_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') { // Тип БД, далее тип таблицы и стандартная кодировка для этой таблицы.
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
